3315f1e00cb095c74fbbbdb361eb205e1b5a5ea6
nischal08 <nischal.karanjit@gmail.com>
2021-08-10 15:57:28 +0545

    created the bird to move and barrier

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flappy_bird/barriers.dart';
 import 'package:flappy_bird/bird.dart';
 import 'package:flutter/material.dart';
 
@@ -11,39 +12,163 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double birdYaxis = -1;
+  static double birdYaxis = 0;
+  double time = 0;
+  double height = 0;
+  double initialHeight = birdYaxis;
+  bool gameHasStarted = false;
+  static double barrierXone = 1;
+  double barrierXtwo = barrierXone + 1.5;
 
   void _jump() {
-    Timer.periodic(Duration(milliseconds: 500), (timer) {
+    setState(() {
+      time = 0;
+      initialHeight = birdYaxis;
+    });
+  }
+
+  void _startGame() {
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 60), (timer) {
+      time += 0.05;
+      height = -4.9 * time * time + 2.8 * time;
       setState(() {
-        birdYaxis += 0.1;
+        birdYaxis = initialHeight - height;
       });
+      setState(() {
+        if (barrierXone < -1.1) {
+          barrierXone += 3.5;
+        } else {
+          barrierXone -= 0.05;
+        }
+      });
+      setState(() {
+        if (barrierXtwo < -1.1) {
+          barrierXtwo += 3.5;
+        } else {
+          barrierXtwo -= 0.05;
+        }
+      });
+      if (birdYaxis > 1) {
+        timer.cancel();
+        gameHasStarted = false;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 2,
-            child: GestureDetector(
-              onTap: _jump,
-              child: AnimatedContainer(
-                alignment: Alignment(0, birdYaxis),
-                color: Colors.blue,
-                duration: Duration(milliseconds: 0),
-                child: MyBird(),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
+    return GestureDetector(
+      onTap: () {
+        if (gameHasStarted) {
+          _jump();
+        } else {
+          _startGame();
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Stack(
+                  children: [
+                    AnimatedContainer(
+                      alignment: Alignment(0, birdYaxis),
+                      color: Colors.blue,
+                      duration: Duration(milliseconds: 0),
+                      child: MyBird(),
+                    ),
+                    Container(
+                      alignment: Alignment(0, -0.3),
+                      child: gameHasStarted
+                          ? Text("")
+                          : Text(
+                              "TAP TO PLAY ",
+                              style: TextStyle(
+                                  letterSpacing: 6,
+                                  fontSize: 20,
+                                  color: Colors.white),
+                            ),
+                    ),
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 0),
+                      alignment: Alignment(barrierXone, 1.1),
+                      child: MyBarrier(
+                        size: 200.0,
+                      ),
+                    ),
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 0),
+                      alignment: Alignment(barrierXone, -1.1),
+                      child: MyBarrier(
+                        size: 200.0,
+                      ),
+                    ),
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 0),
+                      alignment: Alignment(barrierXtwo, 1.1),
+                      child: MyBarrier(
+                        size: 150.0,
+                      ),
+                    ),
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 0),
+                      alignment: Alignment(barrierXtwo, -1.1),
+                      child: MyBarrier(
+                        size: 250.0,
+                      ),
+                    ),
+                  ],
+                )),
+            Container(
+              height: 15,
               color: Colors.green,
             ),
-          )
-        ],
+            Expanded(
+              child: Container(
+                color: Colors.brown,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "SCORE",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "0",
+                          style: TextStyle(color: Colors.white, fontSize: 35),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "BEST",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "20",
+                          style: TextStyle(color: Colors.white, fontSize: 35),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
